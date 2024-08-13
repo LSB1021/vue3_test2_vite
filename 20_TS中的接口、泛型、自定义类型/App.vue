a3d535e3ac901db0bac1b3c8b7de9ff4c82bd770
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-08-13 21:38:10 +0800

    Add files via upload

---
+++ 20_TS中的接口、泛型、自定义类型/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Person/>
+</template>
+
+<script lang="ts" setup>
+  import Person from './components/Person.vue'
+  defineOptions({name:'app'})
+</script>
+
---
+++ 20_TS中的接口、泛型、自定义类型/components/Person.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="person">
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import {type PersonInter,type Persons} from '@/types/index'
+
+    // let person:PersonInter = {id:'asdasd01',name:'张三',age:60}
+
+    let personList:Persons = [
+        {id:'asdasd01',name:'张三',age:60},
+        {id:'asdasd02',name:'李四',age:18},
+        {id:'asdasd03',name:'王五',age:5},
+    ]
+
+
+</script>
+
+
+<style scoped>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        margin:0 5px;
+    }
+</style>
