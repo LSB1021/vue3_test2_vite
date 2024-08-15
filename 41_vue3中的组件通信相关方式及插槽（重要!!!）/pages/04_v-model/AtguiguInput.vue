75fbf6480538ec2564ff5b02195ab6d0146e9173
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-08-15 21:56:25 +0800

    Add files via upload

---
+++ 41_vue3中的组件通信相关方式及插槽（重要!!!）/pages/04_v-model/AtguiguInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input 
+    type="text" 
+    :value="ming"
+    @input="emit('update:ming',(<HTMLInputElement>$event.target).value)"
+  >
+  <br>
+  <input 
+    type="text" 
+    :value="mima"
+    @input="emit('update:mima',(<HTMLInputElement>$event.target).value)"
+  >
+</template>
+
+<script setup lang="ts" name="AtguiguInput">
+  defineProps(['ming','mima'])
+  const emit = defineEmits(['update:ming','update:mima'])
+</script>
+<!-- 
+  (<HTMLInputElement>$event.target).value
+
+  括号内的<HTMLInputElement>$event.target为断言，断言里面的值为HTML里输入的元素，
+  大概也可以写成   $event.target as HTMLInputElement
+-->
+<style scoped>
+  input {
+    border: 2px solid black;
+    background-image: linear-gradient(45deg,red,yellow,green);
+    height: 30px;
+    font-size: 20px;
+    color: white;
+  }
+</style>
